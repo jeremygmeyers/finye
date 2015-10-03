540f4b1d73e494e59e3a3664d485584f52cd6126
jm3519 <jm3519@dyn-160-39-160-100.dyn.columbia.edu>
2015-10-02 23:11:58 -0400

    finishes std, pearson correl & pearson beta

--- script.py
+++ script.py
@@ -9,18 +9,13 @@ import numpy
 
 # TASK LIST
 # 1. CONTEMPLATE SWITCHING TO QUANDL
-    # READ https://blog.thinknewfound.com/2014/06/tracking-down-data/1:
 # 2. ADD MORE FUNCTIONALITY FROM GOOGLE
-# 2A. STDEV
-# 2B. PEARSON CORREL & BETA
+# 2A. STDEV                                DONE
+# 2B. PEARSON CORREL & BETA                DONE
 # 2C. SPEARMAN CORREL & BETA
 
 # Note: Historical TLT data pulled disagrees with Google & Yahoo Finance for much of 2014-15
     # Perhaps this is due to frequent dividend adjustments. Has 12 dividends per year.
-
-# data import, with hardcoded:
-    # end = today
-    # symbols
 
 def import_data(symbols, end_date): # interval 365 days hardcoded
     start = end_date.replace(year=end_date.year-1)
@@ -40,7 +35,14 @@ def daily_returns(data):
 end_date = datetime.date.today()
 symbols = ['SPY','GOOG','IBM','TLT','GLD','^VIX','VXX','UVXY','IWM','RWM','SH']
 data = import_data(symbols, datetime.date.today())
-print data
-print "RETURNS"
-print daily_returns(data)
+returns = daily_returns(data)
 
+returns_std = numpy.std(returns) # stdevs
+returns_corr = returns.corr() # Pearson correlations
+returns_beta = returns_std # easily creates new series of same shape
+market_std = returns_std[0]
+
+for i in xrange(0,len(returns_std)):
+    returns_beta[i] = returns_std[i] * returns_corr.iloc[i,0] / market_std
+
+print returns_beta
