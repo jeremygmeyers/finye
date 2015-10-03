e404629bdfde400cae6e31d11c1d924c01c36165
jm3519 <jm3519@dyn-160-39-160-100.dyn.columbia.edu>
2015-10-03 00:15:02 -0400

    adds function with spearman corr and beta

--- script.py
+++ script.py
@@ -10,9 +10,9 @@ import numpy
 # TASK LIST
 # 1. CONTEMPLATE SWITCHING TO QUANDL
 # 2. ADD MORE FUNCTIONALITY FROM GOOGLE
-# 2A. STDEV                                DONE
-# 2B. PEARSON CORREL & BETA                DONE
-# 2C. SPEARMAN CORREL & BETA
+# 2A. STDEV                                 DONE
+# 2B. PEARSON CORREL & BETA                 DONE
+# 2C. SPEARMAN CORREL & BETA                DONE
 
 # Note: Historical TLT data pulled disagrees with Google & Yahoo Finance for much of 2014-15
     # Perhaps this is due to frequent dividend adjustments. Has 12 dividends per year.
@@ -30,16 +30,30 @@ def daily_returns(data):
     new_data = new_data[1:] # removes top row of NaNs
     return new_data
 
-def collinearity(returns):
+def collinearity_pearson(returns):
     returns_std = numpy.std(returns) # stdevs
     returns_corr = returns.corr() # Pearson correlations
     returns_beta = returns_std.copy() # easily creates new series of same shape
     market_std = returns_std[0]
-
     for i in xrange(0,len(returns_std)):
         returns_beta[i] = returns_std[i] * returns_corr.iloc[i,0] / market_std
-
     return returns_std, returns_corr, returns_beta
+
+def collinearity_spearman(returns):
+    returns_std = numpy.std(returns) # stdevs
+    spearman_corr = returns_std.copy() # easily creates new series of same shape
+    spearman_beta = returns_std.copy() # see above
+    for i in xrange(0,len(returns_std)):
+        temp = returns.iloc[:,i]
+        temp_sort = temp.argsort()
+        ranks = numpy.empty(len(temp), int)
+        ranks[temp_sort] = numpy.arange(len(temp))
+        if i == 0: market_ranks = ranks
+        diff = (ranks - market_ranks)**2
+        spearman_corr[i] = 1- 6*diff.sum() / float((len(diff)) * (len(diff)**2-1))
+        market_std = returns_std[0]
+        spearman_beta[i] = spearman_corr[i] * returns_std[i] / market_std
+    return returns_std, spearman_corr, spearman_beta
 
 # main method
 
@@ -48,21 +62,9 @@ symbols = ['SPY','GOOG','IBM','TLT','GLD','^VIX','VXX','UVXY','IWM','RWM','SH']
 
 data = import_data(symbols, datetime.date.today())
 returns = daily_returns(data)
-returns_std, returns_corr, returns_beta = collinearity(returns)
+returns_std, returns_corr, returns_beta = collinearity_spearman(returns)
 
-
-spearman_corr = returns_std.copy() # easily creates new series of same shape
-spearman_beta = returns_std.copy() # see above
-for i in xrange(0,len(returns_std)):
-    temp = returns.iloc[:,i]
-    temp_sort = temp.argsort()
-    ranks = numpy.empty(len(temp), int)
-    ranks[temp_sort] = numpy.arange(len(temp))
-    if i == 0: market_ranks = ranks
-    diff = (ranks - market_ranks)**2
-    spearman_corr[i] = 1- 6*diff.sum() / float((len(diff)) * (len(diff)**2-1))
-    market_std = returns_std[0]
-    spearman_beta[i] = spearman_corr[i] * returns_std[i] / market_std
+print returns_std, returns_corr, returns_beta
 
 
 
