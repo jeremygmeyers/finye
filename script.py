3976d2f21a38a6b824da18afe2f2743723a46d07
jm3519 <jm3519@Jeremys-MacBook-Air.local>
2016-02-15 00:33:08 -0500

    duplicate commit

--- script.py
+++ script.py
@@ -2,6 +2,7 @@ import pandas
 import pandas_datareader.data as web
 import datetime
 import matplotlib.pyplot as plt
+import matplotlib.dates
 import numpy
 
 # problem - latest 1dmove is incorrect, prob b/c i'm losing last day price
@@ -19,7 +20,7 @@ import numpy
 #       historical analysis
 #       port stats
 #       compare multiple portfolios
-# 4. DOWNLOAD DATA & REUSE (for sans wifi)  undid
+# 4. DOWNLOAD DATA & REUSE (for sans wifi)  in progress
 #       stock data -- done
 #       options data -- wait                in progress!!!
 # 5. MARKET AWARENESS + OPTIONS             in progress
@@ -28,18 +29,28 @@ import numpy
 # 6. DEVELOP INTERFACE
 #       input should be happening interactively, perhaps with web app
 # 7. FURTHER UNDERSTAND THE DATA
+
+# LONG TERM
+# how to calculate delta?
+
+# convert csv from TOS into a different format I can analyze
+
 #       how does div adj happen? does it explain the difference for TLT?
 #       Historical TLT data pulled disagrees with Google & Yahoo Finance for much of 2014-15. Perhaps this is due to frequent dividend adjustments. Has 12 dividends per year.
 
-def import_data(symbols, end_date): # interval 365 days hardcoded
-    start = end_date.replace(year=end_date.year-1)
-    data = pandas.DataFrame()
-    for sym in symbols:
-        data[sym] = web.DataReader(sym, 'yahoo', start, end_date)['Adj Close']
-        last_price = round ( option_data(sym).iloc[0,11] , 2)
-        # on occasion the latest day close will not appear
-        if round ( float ( data.iloc[len(data)-1] ) , 2) != last_price :
-            data = data.append(pandas.Series(last_price,[end_date]).to_frame(sym))
+def import_data(symbols, start_date, end_date): # vary close type!
+    name = ''.join(symbols)+str(start_date)+str(end_date)+'.csv'
+    try:
+        data = pandas.read_csv(name,index_col=0)
+    except IOError:
+        data = pandas.DataFrame()
+        for sym in symbols:
+            data[sym] = web.DataReader(sym, 'yahoo', start_date, end_date)['Adj Close']
+            last_price = round ( option_data(sym).iloc[0,11] , 2)
+            # on occasion the latest day close will not appear
+            if round ( float ( data[sym].iloc[len(data)-1] ) , 2) != last_price :
+                data = data.append(pandas.Series(last_price,[end_date]).to_frame(sym))
+        data.to_csv(name)
     return data
     # Adj Close adjusts for dividends in the close price
 
@@ -50,8 +61,8 @@ def daily_returns(data):
 
 def collinearity(returns,spearman): #will add a symbol for beta/corr calc.
     #assumes returns input are up to date, since compares with uptodate SPY
-    numsym = len(returns.columns)
-    spy = import_data(['SPY'], datetime.date.today() )
+    numsym = len(returns.columns)-1
+    spy = import_data(['SPY'], date_num_years_ago(datetime.date.today(),1), datetime.date.today() )
     returnsSPY = daily_returns(spy)
     returns = pandas.DataFrame(returns,index=returns.index)
     returns = pandas.concat([returns,returnsSPY],axis=1)
@@ -78,7 +89,7 @@ def collinearity(returns,spearman): #will add a symbol for beta/corr calc.
 def correlation_analysis(stockA, stockB, setPrint):
     # if you want stock beta against index, set stockA=index
     # assumes 1 year interval since import_data assumes that
-    loc_data = import_data([stockA,stockB], datetime.date.today())
+    loc_data = import_data([stockA,stockB], date_num_years_ago(datetime.date.today(),1), datetime.date.today())
     loc_returns = daily_returns(loc_data)
 
     loc_stds, loc_pearson_corrs, loc_pearson_betas = collinearity(loc_returns,0)
@@ -91,7 +102,7 @@ def correlation_analysis(stockA, stockB, setPrint):
         result.columns = ['Spearman Beta','Spearman Corr','Pearson Beta','Pearson Corr']
         print 'Spearman Beta & Correlation: \n', result
 
-        loc_data = import_data([stockA,stockB],datetime.date.today())
+        loc_data = import_data([stockA,stockB],date_num_years_ago(datetime.date.today(),1), datetime.date.today())
         loc_returns = daily_returns(loc_data)
         graph = plt.figure()
         plt.grid(True)
@@ -122,7 +133,7 @@ class Portfolio(object):
             self.amounts.append( amounts[x] ) # amount of stock held in each
 
 def historicalsPort(port):
-    data = import_data(port.symbols, datetime.date.today())
+    data = import_data(port.symbols, date_num_years_ago(datetime.date.today(),1), datetime.date.today())
     data.loc[:,'port'] = pandas.Series(0, index=data.index)
     for i in range(0,len(data.iloc[:,0])): # across rows
         for z in range(0,len(data.iloc[0,:])-1): # across cols
@@ -130,14 +141,14 @@ def historicalsPort(port):
     return data
 
 def stockAmount(symbol,value):
-    loc_data = import_data([symbol], datetime.date.today())
+    loc_data = import_data([symbol], date_num_years_ago(datetime.date.today(),1), datetime.date.today())
     loc_price = loc_data.iloc[len(loc_data)-1]
     loc_price = loc_price.iloc[0]
     amount = value / loc_price
     return amount
 
 def stockValue(symbol,amount,daysAgo):
-    loc_data = import_data([symbol], datetime.date.today())
+    loc_data = import_data([symbol], date_num_years_ago(datetime.date.today(),1), datetime.date.today())
     loc_price = loc_data.iloc[len(loc_data)-1-daysAgo]
     loc_price = loc_price.iloc[0]
     value = amount * loc_price
@@ -163,7 +174,7 @@ def portSum(port,toPrint):
 def zeroBetaPortfolio(symbols,value,spearman):
     # this will go long all positions
     # spearman = 0 uses pearson, spearman = 1 uses spearman
-    loc_data = import_data(symbols, datetime.date.today())
+    loc_data = import_data(symbols, date_num_years_ago(datetime.date.today(),1), datetime.date.today())
     loc_returns = daily_returns(loc_data)
     loc_stds, loc_corrs, loc_betas = collinearity(loc_returns,spearman)
     abs_sum_beta = numpy.sum(numpy.absolute(loc_betas))
@@ -267,7 +278,8 @@ def options_analysis(symbols):
     new = pandas.DataFrame(0, index=symbols, columns=['IV','Bid','Ask','Strike','Price','Ratio','Beta','Corr','DayMove','MonthMove','YearMove','pRank52'])
     for x in range(0,len(symbols)):
         data = option_data(symbols[x])
-        data['IV'] = data['IV'].replace('%','',regex=True).astype('float')/100
+        data['IV'] = data['IV'].replace('%','',regex=True)
+        data['IV'] = data['IV'].replace(',','',regex=True).astype('float')/100
         strikes = data.index.get_level_values('Strike')
 
         # get nearest OTM call and output IV, I'd rather us option w/ delta=30, but this is a good proxy for now
@@ -329,18 +341,96 @@ def uppercase(symbols):
 
 #print calculate_iv_on_exp('TLT')
 
-print options_analysis(uppercase(['spy','tgt','gld','tlt','eem','iwm','goog','ibm','yhoo','x','uso','ung','slv','gm','qqq']))
-
+#print options_analysis(uppercase(['spy','tgt','gld','tlt','eem','iwm','goog','ibm','yhoo','x','twtr','ung','gm','qqq']))
 
 def current_price(symbol):
     return round ( option_data(symbol).iloc[0,11] , 2)
 
+def correlation_portfolio(symbols):
+    # get returns data for symbols, ignore amounts of each in portfolio
+    loc_data = import_data(symbols, date_num_years_ago(datetime.date.today(),1), datetime.date.today())
+    loc_returns = daily_returns(loc_data)
 
-# NEXT
-# add in stock data
+    # analyze the correlations against each other
+    corr_table = pandas.DataFrame(index=symbols,columns=symbols)
 
-# LONG TERM
-# how to calculate delta?
+    for x in range(0,len(symbols)):
+        for y in range(0,len(symbols)):
+            # returns pearson correlation
+            corr_table.iloc[x,y] =  numpy.corrcoef(loc_returns.iloc[:,x],
+                    loc_returns.iloc[:,y])[0,1]
+
+    # flips table for easy viewing
+    corr_table = corr_table[::-1]
+    corr_table = corr_table.transpose()
+
+    corr_table.to_csv('corr_table.csv')
+
+    print corr_table
+
+
+def get_correlation(returns,spearman):
+    numsym = len(returns.columns)-1
+    returns_std = numpy.std(returns) # stdevs
+    returns_corr = returns.corr() # Pearson corrs
+    returns_corr = returns.corr().iloc[:,numsym] # Pearson corrs series
+    returns_beta = returns_std.copy() # easily creates new series of same shape
+    market_std = returns_std[numsym]
+    if spearman == 0:
+        for i in range(0, numsym):
+            returns_beta[i] = returns_std[i] * returns_corr.iloc[i] / market_std
+    if spearman == 1:
+        for i in range(numsym,-1,-1):
+            temp = returns.iloc[:,i]
+            temp_sort = temp.argsort()
+            ranks = numpy.empty(len(temp), int)
+            ranks[temp_sort] = numpy.arange(len(temp))
+            if i == numsym: market_ranks = ranks
+            diff = (ranks - market_ranks)**2
+            returns_corr[i] = 1- 6*diff.sum() / float((len(diff)) * (len(diff)**2-1))
+            returns_beta[i] = returns_corr[i] * returns_std[i] / market_std
+    return returns_std.iloc[:-1], returns_corr.iloc[:-1], returns_beta.iloc[:-1]
+
+# Code 30d, 60d, 90d, 180d, 1y correlations between different underlyings and SPY // chart of 1m and 3m  moving correlation
+def corr_plot(symbols,moving_avg_days, start_date, end_date):
+    loc_data = import_data(symbols, start_date, end_date)
+    loc_returns = daily_returns(loc_data)
+    array_of_correlations = []
+    for x in range(0,len(loc_returns)-moving_avg_days):
+        try:
+            array_of_correlations.append(get_correlation(loc_returns[x:x+moving_avg_days],0)[1])
+        except ValueError:
+            donothing = 0
+
+    graph = plt.figure()
+    plt.grid(True)
+    plt.title(str(moving_avg_days) +' day moving Correlation of ' + symbols[0] + ' and ' + symbols[1],fontsize=20,fontweight='bold')
+    '''
+    plt.gca().xaxis.set_major_formatter(matplotlib.dates.DateFormatter('%m/%d/%Y'))
+    dates = []
+    for x in range(moving_avg_days,len(loc_returns)):
+        dates.append(matplotlib.dates.DateFormatter(loc_returns.index[x]))
+    print type(dates)
+    '''
+    plt.scatter(xrange(0,len(array_of_correlations)),array_of_correlations)
+    plt.xlabel("Date", fontsize=20)
+    plt.ylabel("Correlation", fontsize=20)
+
+    plt.show()
+    graph.savefig('graph.jpg')
+    graph.clear()
+    graph.clf()
+    return array_of_correlations
+
+def date_num_years_ago(end_date,num_years_ago):
+    return datetime.date.today().replace(year=datetime.date.today().year-num_years_ago)
+
+corr_plot(['USO','SPY'],40, date_num_years_ago(datetime.date.today(),2), datetime.date.today())
+#corr_plot(['GLD','IBM','IWM','TWTR','USO','X'])
+
+#'BA','BP','BAC','BK','CL','DAL','HON','HOT','NOC','SBUX','HPQ','T','VZ','COST','C','GE','UAL'])
+
+#correlation_portfolio(['GLD','IBM','IWM','TWTR','USO','X'])
 
 # main method
 
@@ -349,8 +439,7 @@ def current_price(symbol):
 #data = import_data(symbols,end_date)
 #returns = daily_returns(data)
 #returns_std, returns_corr, returns_beta = collinearity(returns,0)
-#correlation_analysis('UNG','USO',1)
-#print returns_std, '\n',returns_corr,'\n', returns_beta
+#correlation_analysis('UAL','USO',1)
 
 #x = zeroBetaPortfolio(['IWM','EEM','GLD','TLT'],100000,1)
 #portSum(x,1)
@@ -359,4 +448,3 @@ def current_price(symbol):
 #print comparePorts([x,y])
 #analyzePortfolio(x,1)
 #correlation_analysis('IWM','TLT',1)
-
